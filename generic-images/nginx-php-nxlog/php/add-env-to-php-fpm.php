42c65374bb7522089fe39b4dc6d7b7681211d641
ecto <maxim@zalil.ru>
2016-07-06 21:39:23 -0500

    supervisor setup

--- generic-images/nginx-php-nxlog/php/add-env-to-php-fpm.php
+++ generic-images/nginx-php-nxlog/php/add-env-to-php-fpm.php
@@ -6,6 +6,7 @@ preg_match_all('/(\w+)=(.+)$/m', `env`, $matches);
 
 $conf = "\n";
 foreach($matches[0] as $line => $raw) {
+    if ($matches[1][$line] != "LS_COLORS")
     $conf .= 'env[' . $matches[1][$line] . '] = ' . escapeshellarg($matches[2][$line]) . "\n";
 }
 
--- generic-images/nginx-php-nxlog/start.sh
+++ generic-images/nginx-php-nxlog/start.sh
@@ -3,6 +3,4 @@
 . /opt/nginx-php-nxlog/init-env.sh
 . /opt/nginx-php-nxlog/nxlog/init-nxlog.sh
 
-php5-fpm
-
-nginx
+supervisord -c /opt/nginx-php-nxlog/supervisor.conf
